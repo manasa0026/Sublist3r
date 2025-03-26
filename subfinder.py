60aa1089390b2eb36edc0a60ec33b2346ff75f91
manasa0026 <167519792+manasa0026@users.noreply.github.com>
2025-03-26 20:03:48 +0530

    Update subfinder.py

--- subfinder.py
+++ subfinder.py
@@ -48,7 +48,13 @@ class SubdomainFinder:
     def __init__(self, domain):
         self.domain = domain
         self.subdomains = set()
-        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.headers = {
+            "User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language': 'en-US,en;q=0.8",
+            "Accept-Encoding': 'gzip",
+            }
+        self.print_banner()
 
     def search_google(self):
         url = f"https://www.google.com/search?q=site:{self.domain}"
